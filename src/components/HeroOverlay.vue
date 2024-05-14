f438c4bbe41d9a29ceaeefb7cad54633f4d578d6
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-05-14 12:59:57 +0200

    feat: overlay on hero almost done text moving - transition to adjust

--- src/components/HeroOverlay.vue
+++ src/components/HeroOverlay.vue
@@ -3,12 +3,15 @@
 export default {
     data(){
         return{
-            
+            isOpen: false
         }
     },
 
     methods:{
-    
+        openModal() {
+            this.isOpen = !this.isOpen
+            console.log(this.isOpen);
+        }
     }
 }
 </script>
@@ -19,10 +22,29 @@ export default {
         <div class="row overlay h-100 w-50">
             <div class="col ms_margin h-100 d-flex flex-column justify-content-center">
                 <h5 class="ms-green">WELCOME TO RAGX</h5>
-                <h1 class="text-white fw-bold my-hero-fs">WE FALL. THEN <br> WE RISE.</h1>
-                <div>
-                    <button class="btn p-3 fs-5">Read More &RightArrow;</button>
+                <div id="bg_carousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <h1 class="text-white fw-bold my-hero-fs">WE FALL. THEN <br> WE RISE.</h1>
+                        </div>
+                        <div class="carousel-item">
+                            <h1 class="text-white fw-bold my-hero-fs">YOU SPEAK OF JUSTICE.</h1>
+                        </div>
+                        <div class="carousel-item">
+                            <h1 class="text-white fw-bold my-hero-fs">THEY NOT GONNA WIN.</h1>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="d-flex gap-4 align-items-center">
+                    <button class="btn p-2 py-3 fs-5">Read More &RightArrow; </button>
+                    <button @click="openModal" class="btn ms-btn rounded-circle"><i class="fa-solid fa-play"></i></button>
                     
+                    <div v-if="isOpen">
+                        <div class="ms-modal">
+                            <h1>ciao</h1>
+                        </div>
+                    </div>
                 </div>  
 
             </div>
@@ -44,7 +66,7 @@ export default {
         color: $green
     }
     .my-hero-fs{
-        font-size: 6rem;
+        font-size: 5rem;
     }
     .btn {
         background-color: $light-green;
@@ -55,7 +77,11 @@ export default {
     background: transparent;
     border: 1px solid $light-green;
     color: $light-green;
-}
+    }
+    .ms-btn {
+        height: 80px;
+        width: 80px;
+    }
 }
 
 
