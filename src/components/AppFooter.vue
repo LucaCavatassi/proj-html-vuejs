f37d182e7af67c328e37ed11d81d0aba1b828e51
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-14 11:56:15 +0200

    feat: layout footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,7 +9,41 @@ export default {
     data() {
         return {
             store,
+            teamsArray: [
+                {
+                    src: "1.png",
+                    name: "Logo Ninja Team",
+                },
+                {
+                    src: "2.png",
+                    name: "Logo King Team",
+                },
+                {
+                    src: "3.png",
+                    name: "Logo SkullG Team",
+                },
+                {
+                    src: "4.png",
+                    name: "Logo Panda Team",
+                },
+                {
+                    src: "5.png",
+                    name: "Logo Witch Team",
+                },
+                {
+                    src: "6.png",
+                    name: "Logo Astro Team",
+                },
+            ],
         }
+    },
+    methods: {
+        getImagePath(image) {
+            return new URL(`../assets/img/${image.src}`, import.meta.url).href;
+        },
+        getImageName(image) {
+            return new String(`${image.name}`);
+        },
     },
 }
 </script>
@@ -17,26 +51,89 @@ export default {
 <template>
     <footer>
         <div class="footer-top">
-            <div class="team-icons">
-                <ul>
+            <div class="teams-icons">
+                <ul v-for="team in teamsArray">
                     <li>
-                        <img src="../assets/img/1.png" alt="">
+                        <img :src="getImagePath(team)" :alt="getImageName(team)">
                     </li>
                 </ul>
             </div>
         </div>
+        <div class="footer-center container py-4">
+            <div class="row">
+                <!-- Box footer -->
+                <div class="col">
+                    <img src="../assets/img/menulogo.png" alt="Logo RaxG">
+                    <p>Lorem ipsum dolor sit elit. Consectetur facilis exercitationem,
+                        libero ad dolor sapiente! Sequi quae.</p>
+                    <div class="email-subscribe">
+                        <input type="text" placeholder="Enter Your Email" aria-label="Email">
+                        <button>Subscribe</button>
+                    </div>
+                    <div class="socials-icons">
+                        <span class="bg-success rounded-circle p-1 mx-2">
+                            <i class="fa-brands fa-facebook"></i>
+                        </span class="bg-success rounded-circle p-1 mx-2">
+                        <span class="bg-success rounded-circle p-1 mx-2">
+                            <i class="fa-brands fa-twitter"></i>
+                        </span class="bg-success rounded-circle p-1 mx-2">
+                        <span class="bg-success rounded-circle p-1 mx-2">
+                            <i class="fa-brands fa-instagram"></i>
+                        </span class="bg-success rounded-circle p-1 mx-2">
+                        <span class="bg-success rounded-circle p-1 mx-2">
+                            <i class="fa-brands fa-linkedin"></i>
+                        </span class="bg-success rounded-circle p-1 mx-2">
+                    </div>
+                </div>
+                <!-- /Box footer -->
 
-        <ul>
-            <li v-for="item in store.menu" :key="item">
-                <router-link :to="{ name: item.routeName }">
-                    {{ item.title }}
-                </router-link>
-            </li>
-            <li>
-                <!-- TEST -->
-                <h1>{{ storeObj.menu[2].title }}</h1>
-            </li>
-        </ul>
+                <!-- Lista quick links (menu) -->
+                <div class="col">
+                    <h3>Quick Links</h3>
+                    <ul v-for="item in storeObj.menu">
+                        <li>
+                            <router-link :to="{ name: item.routeName }">
+                                {{ item.title }}
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /Lista quick links (menu) -->
+
+                <!-- Lista services -->
+                <div class="col">
+                    <h3>Our Services</h3>
+                    <ul v-for="item in storeObj.services">
+                        <li>
+                            <router-link :to="{ name: item.routeName }">
+                                {{ item.title }}
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /Lista services -->
+
+                <!-- Lista contacts -->
+                <div class="col">
+                    <h3>Contact Us</h3>
+                    <ul v-for="item in storeObj.contacts">
+                        <li>
+                            <i :class="`fa-solid ${item.path_icon}`"></i>
+                            <router-link :to="{ name: item.routeName }">
+                                {{ item.title }}
+
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /Lista contacts -->
+            </div>
+
+        </div>
+
+        <div class="footer-bottom text-center py-4 bg-dark fw-bold">
+            <p class="text-white">Copyright © RaxG | Designed by <span class="text-success">NextGenerationDev</span></p>
+        </div>
     </footer>
 </template>
 
@@ -48,12 +145,16 @@ footer {
 
     .footer-top {
 
-        .team-icons {
+        .teams-icons {
             width: 80%;
             display: flex;
             justify-content: center;
             background-color: lightseagreen;
             margin: 0 auto;
+
+            img {
+                max-width: 80%;
+            }
         }
     }
 }
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { reactive } from "vue";
 
 export const store = reactive({
   menu: [
+    // per evitare errori, i routeName delle pagine non previste dal progetto (es. blog) puntano tutte su HOME
     {
       title: "Home",
       routeName: "home",
@@ -14,11 +15,65 @@ export const store = reactive({
       title: "Contact",
       routeName: "contact",
     },
+    {
+      title: "Blog",
+      routeName: "home",
+    },
+    {
+      title: "Shop",
+      routeName: "home",
+    },
+    {
+      title: "Pages",
+      routeName: "home",
+    },
   ],
   services: [
     {
       title: "About Us",
-      routeName: "test",
+      routeName: "home",
+    },
+    {
+      title: "Latest Turnament",
+      routeName: "home",
+    },
+    {
+      title: "Uocoming Game",
+      routeName: "home",
+    },
+    {
+      title: "Faqs",
+      routeName: "home",
+    },
+    {
+      title: "Privacy",
+      routeName: "home",
+    },
+    {
+      title: "Terms & Conditions",
+      routeName: "home",
+    },
+  ],
+  contacts: [
+    {
+      title: "69 Broklyn Street, New York city, United States of America,",
+      routeName: "home",
+      path_icon: "fa-location-pin",
+    },
+    {
+      title: "+880 222 333 2580",
+      routeName: "home",
+      path_icon: "fa-phone",
+    },
+    {
+      title: "example@gmail.com",
+      routeName: "home",
+      path_icon: "fa-envelope",
+    },
+    {
+      title: "www.example.com",
+      routeName: "home",
+      path_icon: "fa-globe",
     },
   ],
 });
