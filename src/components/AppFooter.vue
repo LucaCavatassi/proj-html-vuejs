22de77a93fdfbbb071bb4d0792ad18135863beaf
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-14 18:37:55 +0200

    feat: footer partial graphic layout

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -50,113 +50,139 @@ export default {
 
 <template>
     <footer class="pt-5">
-        <div class="footer-top">
-            <div class="teams-icons">
-                <ul v-for="team in teamsArray">
-                    <li>
+        <div class="ms_footer-top">
+            <div class="container rounded">
+                <div class="row align-items-center">
+                    <div class="col ms_teams-icons p-4" v-for="team in teamsArray">
                         <img :src="getImagePath(team)" :alt="getImageName(team)">
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="footer-center container py-4">
-            <div class="row">
-                <!-- Box footer -->
-                <div class="col">
-                    <img src="../assets/img/menulogo.png" alt="Logo RaxG">
-                    <p>Lorem ipsum dolor sit elit. Consectetur facilis exercitationem,
-                        libero ad dolor sapiente! Sequi quae.</p>
-                    <div class="email-subscribe d-flex pb-4">
-                        <input type="text" placeholder="Enter Your Email" aria-label="Email">
-                        <button>Subscribe</button>
-                    </div>
-                    <div class="socials-icons">
-                        <span class="bg-success rounded-circle p-1 mx-2">
-                            <i class="fa-brands fa-facebook"></i>
-                        </span class="bg-success rounded-circle p-1 mx-2">
-                        <span class="bg-success rounded-circle p-1 mx-2">
-                            <i class="fa-brands fa-twitter"></i>
-                        </span class="bg-success rounded-circle p-1 mx-2">
-                        <span class="bg-success rounded-circle p-1 mx-2">
-                            <i class="fa-brands fa-instagram"></i>
-                        </span class="bg-success rounded-circle p-1 mx-2">
-                        <span class="bg-success rounded-circle p-1 mx-2">
-                            <i class="fa-brands fa-linkedin"></i>
-                        </span class="bg-success rounded-circle p-1 mx-2">
                     </div>
                 </div>
-                <!-- /Box footer -->
-
-                <!-- Lista quick links (menu) -->
-                <div class="col">
-                    <h3>Quick Links</h3>
-                    <ul v-for="item in storeObj.menu">
-                        <li>
-                            <router-link :to="{ name: item.routeName }">
-                                {{ item.title }}
-                            </router-link>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /Lista quick links (menu) -->
-
-                <!-- Lista services -->
-                <div class="col">
-                    <h3>Our Services</h3>
-                    <ul v-for="item in storeObj.services">
-                        <li>
-                            <router-link :to="{ name: item.routeName }">
-                                {{ item.title }}
-                            </router-link>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /Lista services -->
-
-                <!-- Lista contacts -->
-                <div class="col">
-                    <h3>Contact Us</h3>
-                    <ul v-for="item in storeObj.contacts">
-                        <li>
-                            <i :class="`fa-solid ${item.path_icon}`"></i>
-                            <router-link :to="{ name: item.routeName }">
-                                {{ item.title }}
-
-                            </router-link>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /Lista contacts -->
             </div>
 
         </div>
+        <div class="ms_footer-center">
+            <div class="container position-relative z-2">
+                <div class="row text-white">
+                    <!-- Box footer -->
+                    <div class="col">
+                        <img src="../assets/img/menulogo.png" alt="Logo RaxG">
+                        <p>Lorem ipsum dolor sit elit. Consectetur facilis exercitationem,
+                            libero ad dolor sapiente! Sequi quae.</p>
+                        <div class="email-subscribe d-flex pb-4">
+                            <input type="text" placeholder="Enter Your Email" aria-label="Email">
+                            <button>Subscribe</button>
+                        </div>
+                        <div class="socials-icons">
+                            <span class="bg-success rounded-circle p-1 mx-2">
+                                <i class="fa-brands fa-facebook"></i>
+                            </span class="bg-success rounded-circle p-1 mx-2">
+                            <span class="bg-success rounded-circle p-1 mx-2">
+                                <i class="fa-brands fa-twitter"></i>
+                            </span class="bg-success rounded-circle p-1 mx-2">
+                            <span class="bg-success rounded-circle p-1 mx-2">
+                                <i class="fa-brands fa-instagram"></i>
+                            </span class="bg-success rounded-circle p-1 mx-2">
+                            <span class="bg-success rounded-circle p-1 mx-2">
+                                <i class="fa-brands fa-linkedin"></i>
+                            </span class="bg-success rounded-circle p-1 mx-2">
+                        </div>
+                    </div>
+                    <!-- /Box footer -->
 
-        <div class="footer-bottom text-center py-4 bg-dark fw-bold">
+                    <!-- Lista quick links (menu) -->
+                    <div class="col">
+                        <h3>Quick Links</h3>
+                        <ul v-for="item in storeObj.menu">
+                            <li>
+                                <router-link :to="{ name: item.routeName }">
+                                    {{ item.title }}
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /Lista quick links (menu) -->
+
+                    <!-- Lista services -->
+                    <div class="col">
+                        <h3>Our Services</h3>
+                        <ul v-for="item in storeObj.services">
+                            <li>
+                                <router-link :to="{ name: item.routeName }">
+                                    {{ item.title }}
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /Lista services -->
+
+                    <!-- Lista contacts -->
+                    <div class="col">
+                        <h3>Contact Us</h3>
+                        <ul v-for="item in storeObj.contacts">
+                            <li>
+                                <i :class="`fa-solid ${item.path_icon}`"></i>
+                                <router-link :to="{ name: item.routeName }">
+                                    {{ item.title }}
+
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /Lista contacts -->
+                </div>
+            </div>
+            <div class="ms_overlay"></div>
+        </div>
+
+
+
+        <div class="ms_footer-bottom text-center py-4 fw-bold">
             <p class="text-white">Copyright © RaxG | Designed by <span class="text-success">NextGenerationDev</span></p>
         </div>
     </footer>
 </template>
 
 <style scoped lang="scss">
-// debug
+@use "../style/partials/variables" as *;
+
 footer {
-    background-color: lightslategray;
+    background-color: $dark-purple;
 
-    .footer-top {
-        // da sistemare
-        transform: translateY(-50px);
+    .ms_footer-top {
+        position: relative;
+        top: 50px;
+        z-index: 2;
 
-        .teams-icons {
-            width: 80%;
-            display: flex;
-            justify-content: center;
-            background-color: lightseagreen;
-            margin: 0 auto;
+        .container {
+            background-color: $purple;
+        }
 
+        .ms_teams-icons {
             img {
-                max-width: 80%;
+                max-width: 100px;
             }
         }
+    }
+
+    .ms_footer-center {
+        background-image: url('../assets/img/footer-bg.png');
+        background-size: cover;
+        position: relative;
+        padding-top: 10em;
+        padding-bottom: 4em;
+
+        .ms_overlay {
+            background-color: rgba(32, 32, 70, 0.50);
+            position: absolute;
+            top: 0;
+            z-index: 1;
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    .ms_footer-bottom {
+        background-color: $purple;
     }
 }
 </style>
