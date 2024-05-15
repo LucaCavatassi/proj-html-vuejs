cd8fa8e120afe71ebbfdb828f06a44fd4a297866
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-15 10:39:09 +0200

    feat: footer layout completed

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -62,71 +62,83 @@ export default {
         </div>
         <div class="ms_footer-center">
             <div class="container position-relative z-2">
-                <div class="row text-white">
+                <div class="row text-white px-5 g-5">
                     <!-- Box footer -->
                     <div class="col">
                         <img src="../assets/img/menulogo.png" alt="Logo RaxG">
-                        <p>Lorem ipsum dolor sit elit. Consectetur facilis exercitationem,
-                            libero ad dolor sapiente! Sequi quae.</p>
+                        <p>Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id
+                            enim. Mauris blandit aliquet elit</p>
                         <div class="email-subscribe d-flex pb-4">
                             <input type="text" placeholder="Enter Your Email" aria-label="Email">
                             <button>Subscribe</button>
                         </div>
                         <div class="socials-icons">
-                            <span class="bg-success rounded-circle p-1 mx-2">
+                            <span class="rounded-circle">
                                 <i class="fa-brands fa-facebook"></i>
-                            </span class="bg-success rounded-circle p-1 mx-2">
-                            <span class="bg-success rounded-circle p-1 mx-2">
+                            </span class="rounded-circle">
+                            <span class="rounded-circle">
                                 <i class="fa-brands fa-twitter"></i>
-                            </span class="bg-success rounded-circle p-1 mx-2">
-                            <span class="bg-success rounded-circle p-1 mx-2">
+                            </span class="rounded-circle">
+                            <span class="rounded-circle">
                                 <i class="fa-brands fa-instagram"></i>
-                            </span class="bg-success rounded-circle p-1 mx-2">
-                            <span class="bg-success rounded-circle p-1 mx-2">
+                            </span class="rounded-circle">
+                            <span class="rounded-circle">
                                 <i class="fa-brands fa-linkedin"></i>
-                            </span class="bg-success rounded-circle p-1 mx-2">
+                            </span class="rounded-circle">
                         </div>
                     </div>
                     <!-- /Box footer -->
 
                     <!-- Lista quick links (menu) -->
                     <div class="col">
-                        <h3>Quick Links</h3>
-                        <ul v-for="item in storeObj.menu">
-                            <li>
-                                <router-link :to="{ name: item.routeName }">
-                                    {{ item.title }}
-                                </router-link>
-                            </li>
-                        </ul>
+                        <div class="row">
+                            <h3 class="pb-2">Quick Links</h3>
+                            <ul v-for="item in storeObj.menu">
+                                <li>
+                                    <router-link :to="{ name: item.routeName }"
+                                        class="text-white text-decoration-none fw-semibold">
+                                        {{ item.title }}
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
                     <!-- /Lista quick links (menu) -->
 
                     <!-- Lista services -->
                     <div class="col">
-                        <h3>Our Services</h3>
-                        <ul v-for="item in storeObj.services">
-                            <li>
-                                <router-link :to="{ name: item.routeName }">
-                                    {{ item.title }}
-                                </router-link>
-                            </li>
-                        </ul>
+                        <div class="row">
+                            <h3 class="pb-2">Our Services</h3>
+                            <ul v-for="item in storeObj.services">
+                                <li>
+                                    <router-link :to="{ name: item.routeName }"
+                                        class="text-white text-decoration-none fw-semibold">
+                                        {{ item.title }}
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
                     <!-- /Lista services -->
 
                     <!-- Lista contacts -->
                     <div class="col">
-                        <h3>Contact Us</h3>
-                        <ul v-for="item in storeObj.contacts">
-                            <li>
-                                <i :class="`fa-solid ${item.path_icon}`"></i>
-                                <router-link :to="{ name: item.routeName }">
-                                    {{ item.title }}
+                        <div class="row">
+                            <h3 class="pb-2">Contact Us</h3>
+                            <ul v-for="item in storeObj.contacts">
+                                <li class="d-flex align-items-start">
+                                    <i :class="`fa-solid ${item.path_icon}`"></i>
+                                    <router-link :to="{ name: item.routeName }"
+                                        class="text-white text-decoration-none fw-bold">
+                                        {{ item.title }}
 
-                                </router-link>
-                            </li>
-                        </ul>
+                                    </router-link>
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
                     <!-- /Lista contacts -->
                 </div>
@@ -137,7 +149,7 @@ export default {
 
 
         <div class="ms_footer-bottom text-center py-4 fw-bold">
-            <p class="text-white">Copyright © RaxG | Designed by <span class="text-success">NextGenerationDev</span></p>
+            <p class="text-white">Copyright © RaxG | Designed by <span>NextGenerationDev</span></p>
         </div>
     </footer>
 </template>
@@ -179,10 +191,58 @@ footer {
             width: 100%;
             height: 100%;
         }
+
+        .email-subscribe {
+
+            input {
+                background-color: $dark-purple;
+                border-radius: 10px 0 0 10px;
+            }
+
+            button {
+                background-color: $light-green;
+                border-radius: 0 10px 10px 0;
+                color: $dark-purple;
+                font-weight: 500;
+            }
+
+            input,
+            button {
+                border: 0;
+                padding: .7em;
+            }
+        }
+
+        .socials-icons {
+
+            span {
+                background-color: $violet;
+                padding: .8em;
+                margin-right: .5em;
+
+            }
+        }
+
+        ul {
+            list-style-type: none;
+
+
+            i {
+                color: $green;
+                padding-right: .5em;
+                font-size: 1.5rem;
+            }
+        }
     }
 
     .ms_footer-bottom {
         background-color: $purple;
+
+        span {
+            color: $green;
+
+
+        }
     }
 }
 </style>
