98b515d52b020a5b94bbe2d5222379f1540a9d21
Denis-Turbatu <turbatudenis81@gmail.com>
2024-05-15 13:44:42 +0200

    feat: styled AppFeatures

--- src/components/AppFeature.vue
+++ src/components/AppFeature.vue
@@ -36,13 +36,12 @@ export default {
 </script>
 
 <template>
-    <div class="bg-danger">
+    <div class="bg-features">
         <!-- container -->
-        <div class="container">
-            
+        <div class="container py-200">
             <!-- inner container left -->
-            <div class="w-50">
-                <section>
+            <div class="w-50 position-relative z-2">
+                <section class="text-white">
                     <h6 class="fw-semibold text-success">Our Extrem Feature</h6>
                     <h2 class="fw-semibold fs-50 mb-4">The Best Gaming Arena On The World!</h2>
                     <p>
@@ -54,16 +53,19 @@ export default {
             </div>
 
             <!-- inner container right -->
-            <div>
-                <div class="card" v-for="card in cards" :key="card">
+            <div class="card-container col-6 position-relative z-2">
+                <div v-for="card in cards" :key="card" class="col-6 p-2">
                     <!-- img circle -->
-                    <div class="ms-bg-purple">
-                        <img :src="`http://localhost:5173/src/assets/img/${card.imgUrl}.png`" class="img-fluid" :alt="element">
-                    </div>
-                    <!-- card body -->
-                    <div class="card-body">
-                        <h3 class="card-title">{{ card.title }}</h3>
-                        <p class="card-text">{{ card.description }}</p>
+                    <div class="ms-card ms-bg-light-violet text-white p-4 rounded-4">
+                        <div class="ms-lg-icon ms-bg-purple p-3 rounded-circle">
+                            <img :src="`http://localhost:5173/src/assets/img/${card.imgUrl}.png`" class="img-fluid"
+                                :alt="element">
+                        </div>
+                        <!-- card body -->
+                        <div class="card-body w-50">
+                            <h3 class="card-title text-center">{{ card.title }}</h3>
+                            <p class="card-text text-center">{{ card.description }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -74,4 +76,40 @@ export default {
 
 <style lang="scss">
 @use "../style/partials/variables" as *;
+@use "../style/partials/mixin" as *;
+
+.bg-features{
+    background-color: $light-violet;
+    background-image: url("../assets/img/Vector.png");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: relative;
+
+
+    .container{
+        @include flex(row, center, center)
+    }
+
+    .card-container{
+        @include flex(row, center, center);
+        flex-wrap: wrap;
+        padding: 25px;
+        .ms-card{
+            @include flex(column, center, center);
+        }
+    }
+}
+
+.bg-features::before {
+    position: absolute;
+    content: "";
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba($color: black, $alpha: 0.5);
+    z-index: 1;
+    border-radius: 15px;
+}
 </style>
