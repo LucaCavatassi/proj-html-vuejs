8b78f69b5de73bdf2792e60d82af6648f699e581
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-15 10:45:00 +0200

    feat: export map in a indipendent component

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,12 +1,14 @@
 <script>
 
 import AppHero from '../components/AppHero.vue';
+import AppMaps from '../components/AppMaps.vue';
 import { store } from '../store';
 
 
 export default {
     components: {
         AppHero,
+        AppMaps,
     },
     data() {
         return {
@@ -198,15 +200,9 @@ export default {
             </div>
         </div>
 
-        <div class="contaner-fluid pb-5">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9628136.222063737!2d-109.85605184445477!3d42.707770787818745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sStati%20Uniti!5e0!3m2!1sit!2sit!4v1715700723948!5m2!1sit!2sit"
-                style="border:0; width: 100%; height: 450px;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade">
-            </iframe>
-        </div>
     </main>
 
+    <AppMaps />
 
 </template>
 
